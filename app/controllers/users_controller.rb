27a6e2fbc953549ea8e73f7467a5fb73b2d0ab34
Alex Stomp <stompy1208@gmail.com>
2014-11-21 08:53:00 -0600

    simple page structure

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def dash
+  end
+  
+  def cal
+  end
+
+  def growth
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root 'application#landing'
+
+  #user zone
+  scope 'u' do 
+    get "dash" => "users#dash", :as => :dash
+    get "cal" => "users#cal", :as => :cal
+    get "growth" => "users#growth", :as => :growth
+  end
 end
